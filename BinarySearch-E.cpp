4ed9affde10c77ddd55edfa55e8f9ab004fdfc39
K N Chiranthan <chiranthangowda41@gmail.com>
2025-07-23 18:52:26 +0530

    Problem 540 Optimal Approach using Binary Search

--- BinarySearch-E.cpp
+++ BinarySearch-E.cpp
@@ -86,3 +86,30 @@ using namespace std;
         }
         return -1;
     }*/
+
+    /*Optimal Approach-Binary Search*/
+    /*int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1){return nums[0];}//exceptional case
+        if(nums[0]!=nums[1]){return nums[0];}
+        if(nums[n-1]!=nums[n-2]){return nums[n-1];}
+        int low=1,high=n-2;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1])
+            {
+                return nums[mid];
+            }
+            if(((mid%2)==1 && nums[mid]==nums[mid-1]) || 
+            (mid%2)==0 && nums[mid]==nums[mid+1])
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return -1;
+    }*/
