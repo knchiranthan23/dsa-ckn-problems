9c19fdbeb406c8be0d86c6639ebcce4177871639
K N Chiranthan <chiranthangowda41@gmail.com>
2025-07-24 19:49:02 +0530

    Problem 162. Find peak Element solution in both BF and optimal Approach

--- BinarySearch-E.cpp
+++ BinarySearch-E.cpp
@@ -113,3 +113,41 @@ using namespace std;
         }
         return -1;
     }*/
+
+/*162.Find Peak Element-BT Approach*/
+/*    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if((i==0 || nums[i-1]<nums[i]) && 
+            (i==n-1 || nums[i]>nums[i+1]))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }*/
+
+/*162.Find Peak Element-Optimal Approach*/
+/*    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        int low=0,high=n-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if((mid==0 || nums[mid-1]<nums[mid]) 
+            && (mid==n-1 || nums[mid]>nums[mid+1]))
+            {
+                return mid;
+            }
+            else if(mid>0 && nums[mid-1]>nums[mid])
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return -1;
+    }*/
