b28247aa5397eee27cadd5063cabc13dbc4c9867
K N Chiranthan <chiranthangowda41@gmail.com>
2025-08-23 16:53:47 +0530

    Added leetcode 74. Search a 2D Matrix ptimal Approach

--- BSonAnsand2D.cpp
+++ BSonAnsand2D.cpp
@@ -115,3 +115,48 @@
         return false;
     }*/
 
+/*Optimal Approach TC~O(log(n*m))*/
+/*bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size(),m=matrix[0].size();
+        int low=0,high=n*m-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(matrix[row][col]==target)
+            {
+                return true;
+            }
+            else if(matrix[row][col]<target)
+            {
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }*/
+
+/*Leetcode 240. Search a 2D Matrix II ~ TC()*/
+/*    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size(),m=matrix[0].size();
+        int row=0,col=m-1;
+        int mid;
+        while(row<n && col>=0)
+        {
+           if(matrix[row][col]==target)
+           {
+            return true;
+           }
+           else if(matrix[row][col]<target)
+           {
+              row++;
+           }
+           else{
+            col--;
+           }
+        }
+        return false;
+    }*/
