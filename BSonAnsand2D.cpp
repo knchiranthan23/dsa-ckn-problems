e247a9fedd61160b5f2a3e77e6abff49c2083f37
K N Chiranthan <chiranthangowda41@gmail.com>
2025-08-23 18:42:29 +0530

    Added Leetcode 1901. Find a Peak Element II BT an d Optimal Approach

--- BSonAnsand2D.cpp
+++ BSonAnsand2D.cpp
@@ -139,7 +139,7 @@
         return false;
     }*/
 
-/*Leetcode 240. Search a 2D Matrix II ~ TC()*/
+/*Leetcode 240. Search a 2D Matrix II ~ TC(O(n*m))*/
 /*    bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size(),m=matrix[0].size();
         int row=0,col=m-1;
@@ -159,4 +159,60 @@
            }
         }
         return false;
+    }*/
+
+/*Leetcode 1901. Find a Peak Element II Brute Force Approach*/
+/*    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+       vector<int>res;
+       int n=mat.size(),m=mat[0].size();
+       int larg=INT_MIN;
+       for(int i=0;i<n;i++)
+       {
+        for(int j=0;j<m;j++)
+        {
+           if(mat[i][j]>larg)
+           {
+             larg=mat[i][j];
+             res={i,j};
+           }
+        } 
+      } 
+       return res;
+    }*/    
+/*Leetcode 1901. Find a Peak Element II Optimal Approach*/
+/*   int Findmaxele(vector<vector<int>>& arr,int n, int col)
+    {
+        int maxy=INT_MIN,indx;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i][col]>maxy)
+            {
+                maxy=arr[i][col];
+                indx=i;
+            }
+        }
+        return indx;
+    }
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+       int n=mat.size(),m=mat[0].size();
+       int low=0,high=m-1;
+       while(low<=high)
+       {
+         int mid=(low+high)/2;
+         int row=Findmaxele(mat,n,mid);
+         int left=mid-1>=0 ? mat[row][mid-1]:-1;
+         int right=mid+1<m ? mat[row][mid+1]:-1;
+         if(left<mat[row][mid] && mat[row][mid]>right )
+         {
+            return {row,mid};
+         }
+         else if(left>mat[row][mid])
+         {
+           high=mid-1;
+         }
+         else {
+            low=mid+1;
+         }
+       }
+       return {-1,-1};
     }*/
