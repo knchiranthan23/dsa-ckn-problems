c868e6c063d406bb6589b942e999d170f6a9e2b7
K N Chiranthan <chiranthangowda41@gmail.com>
2025-08-26 18:57:41 +0530

    Added 151. Reverse Words in a String

--- stringsleetcode.cpp
+++ stringsleetcode.cpp
@@ -42,3 +42,52 @@
 //         }
 //         return res;
 //     }   
+
+/*151. Reverse Words in a String*/
+/*Approach 1*/
+// string reverseWords(string s) {
+//         vector<string>words;
+//         string curw;
+//         string res="";
+//         for(char c:s)
+//         {
+//             if(c!=' ')
+//             {
+//                 curw.push_back(c);
+//             }
+//             else if(!curw.empty()){
+//                words.push_back(curw);
+//                curw="";
+//             }
+//         }
+//         if(!curw.empty()) words.push_back(curw);
+//         reverse(words.begin(),words.end());
+//         for(int i=0;i<words.size();i++)
+//         {
+//             res.append(words[i]);
+//             if(i<words.size()-1){
+//                 res.append(" ");
+//             }
+//         }
+//         return res;
+//     }
+
+/*Approach 2*/
+/*reverse(s.begin(),s.end());
+       string ans="";
+       for(int i=0;i<s.length();i++)
+       {
+          string word="";
+          while(i<s.length() && s[i]!=' ')
+          {
+            word+=s[i];
+            i++;
+          } 
+          reverse(word.begin(),word.end());
+          if(word.length()>0)
+          {
+            ans+=" "+word;
+          }
+       }
+       return ans.substr(1);
+    }*/
