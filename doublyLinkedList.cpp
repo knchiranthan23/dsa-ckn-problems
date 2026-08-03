4d7986264213ab74e7752d7401d58446baca2fd8
K N Chiranthan <chiranthangowda41@gmail.com>
2026-08-03 23:34:04 +0530

    Convert array into 2DLinkedList

---
+++ doublyLinkedList.cpp
@@ -0,0 +1,50 @@
+/*Convert array into 2DLinkedList*/
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+//     Node* prev;
+    
+//     public:
+//     Node(int data1,Node* next1,Node* prev1)
+//     {
+//         data = data1;
+//         next = next1;
+//         prev = prev1;
+//     }
+//     Node(int data1)
+//     {
+//         data = data1;
+//         next = nullptr;
+//         prev = nullptr;
+//     }
+// };
+// Node* convert2D(vector<int> &arr)
+// {
+//     Node* head = new Node(arr[0]);
+//     Node* tempprev = head;
+//     for(int i=1;i<arr.size();i++)
+//     {
+//         Node* temp = new Node(arr[i],nullptr,tempprev);
+//         tempprev->next = temp;
+//         tempprev = temp;
+//     }
+//     return head;
+// }
+// void print(Node* head)
+// {
+//     while(head!=NULL)
+//     {
+//         cout<<head->data<<" ";
+//         head = head->next;
+//     }
+// }
+// int main()
+// {
+//     vector<int>arr={1,3,5,100};
+//     Node* head = convert2D(arr);
+//     print(head);
+// }
